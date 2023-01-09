453ee2d3754b6fa47323a7d0b80b95866a3172d0
V-perseus <mangojamstar0513@outlook.com>
2023-01-09 10:56:49 -0500

    updated result

--- rust/system.rs
+++ rust/system.rs
@@ -1,52 +1,34 @@
-use std::io::Read;
-use std::process::{Command, Stdio};
 use std::sync::{Arc, Mutex};
 use std::thread;
-
-/// Pipe streams are blocking, we need separate threads to monitor them without blocking the primary thread.
-fn child_stream_to_vec<R>(mut stream: R) -> Arc<Mutex<Vec<u8>>>
-where
-    R: Read + Send + 'static,
-{
-    let out = Arc::new(Mutex::new(Vec::new()));
-    let vec = out.clone();
-    thread::Builder::new()
-        .name("child_stream_to_vec".into())
-        .spawn(move || loop {
-            let mut buf = [0];
-            match stream.read(&mut buf) {
-                Err(err) => {
-                    println!("{}] Error reading from stream: {}", line!(), err);
-                    break;
-                }
-                Ok(got) => {
-                    if got == 0 {
-                        break;
-                    } else if got == 1 {
-                        vec.lock().expect("!lock").push(buf[0])
-                    } else {
-                        println!("{}] Unexpected number of bytes: {}", line!(), got);
-                        break;
-                    }
-                }
-            }
-        })
-        .expect("!thread");
-    out
-}
+use std::time::Duration;
 
 fn main() {
-    let mut cat = Command::new("cat")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .expect("!cat");
+// This is a variable to be monitored.
+let counter = Arc::new(Mutex::new(0));
 
-    let out = child_stream_to_vec(cat.stdout.take().expect("!stdout"));
-    let err = child_stream_to_vec(cat.stderr.take().expect("!stderr"));
-    let mut stdin = match cat.stdin.take() {
-        Some(stdin) => stdin,
-        None => panic!("!stdin"),
-    };
+let counter2 = Arc::clone(&counter);
+// This is a spawned thread
+// @role: modify variable to 500 after 0.1sec.
+thread::spawn(move || {
+thread::sleep(Duration::from_millis(100));
+let mut num = counter2.lock().unwrap();
+*num == 500;
+println!("spawn thread result: {}", *num );
+});
+
+
+// This is main thread that traces 'counter' variable every 0.01sec.
+loop {
+let ref_counter = *counter.lock().unwrap();
+
+println!("main thread Result: {}", ref_counter);
+
+if ref_counter == 500 {
+println!("OK, that's enough");
+
+// Exit this loop
+break;
+}
+thread::sleep(Duration::from_millis(10));
+}
 }
